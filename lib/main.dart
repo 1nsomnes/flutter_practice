584212b5481592cb1a8704dfb1e979752cd5bc79
Cedric <claecedric@gmail.com>
2024-09-29 15:54:53 -0700

    toggle theme

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_practice/theme/theme_provider.dart';
+import 'package:provider/provider.dart';
 import 'pages/home_page.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(ChangeNotifierProvider(
+    create: (context) => ThemeProvider(),
+    child: const MyApp(),
+  ));
 }
 
 class MyApp extends StatelessWidget {
@@ -11,8 +16,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: HomePage(),
-    );
+        debugShowCheckedModeBanner: false,
+        home: const HomePage(),
+        theme: Provider.of<ThemeProvider>(context).themeData);
   }
 }
--- lib/theme/theme_provider.dart
+++ lib/theme/theme_provider.dart
@@ -7,4 +7,14 @@ class ThemeProvider extends ChangeNotifier {
   ThemeData get themeData => _themeData;
 
   bool get isDarkMode => _themeData == darkMode;
+
+  set themeData(ThemeData themeData) {
+    _themeData = themeData;
+    notifyListeners();
+  }
+
+  void toggleTheme() {
+    _themeData = isDarkMode ? lightMode : darkMode;
+    notifyListeners();
+  }
 }
